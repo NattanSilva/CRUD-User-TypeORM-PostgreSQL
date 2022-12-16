65ba0feb17d1de3fba02ad668662e147caba017e
NattanSilva <natanxboy@gmail.com>
2022-12-16 14:18:12 -0300

    Add: user routes, services and controlelrs

--- src/server.ts
+++ src/server.ts
@@ -7,13 +7,13 @@ const PORT = process.env.SERVER_PORT;
 (async () => {
   await AppDataSource.initialize()
     .then((res) => {
-      console.log("Database conected");
+      console.log("Database conected 🟢");
     })
     .catch((err) => {
-      console.error("Error during Data Source initialization", err);
+      console.error("Error during Data Source initialization 🔴", err);
     });
 
   app.listen(PORT, () => {
-    console.log(`Server running at: http://localhost:${PORT}`);
+    console.log(`Server running at: http://localhost:${PORT} 🚀`);
   });
 })();
